eba0bb622ce31bffcf1680ede24e9e1c31991c78
Samuli Seppänen <samuli@openvpn.net>
2015-10-05 12:10:32 +0000

    Fix $ssldir on FreeBSD + Puppet 4
    Signed-off-by: Samuli Seppänen <samuli@openvpn.net>

--- manifests/params.pp
+++ manifests/params.pp
@@ -9,7 +9,6 @@ class puppetagent::params {
 
     # These are Puppet 4 defaults. We would not need these if it were not for 
     # other modules which depends on them.
-    $ssldir = '/etc/puppetlabs/puppet/ssl'
     $rundir = '/var/run/puppetlabs'
 
     case $::osfamily {
@@ -17,16 +16,21 @@ class puppetagent::params {
             $config_file = '/etc/puppetlabs/puppet/puppet.conf'
             $admingroup = 'root'
             $service_name = 'puppet'
+            # This assume we're using official Puppetlabs Puppet 4 packages
+            $ssldir = '/etc/puppetlabs/puppet/ssl'
         }
         'Debian': {
             $config_file = '/etc/puppetlabs/puppet/puppet.conf'
             $admingroup = 'root'
             $service_name = 'puppet'
+            $ssldir = '/etc/puppetlabs/puppet/ssl'
         }
         'FreeBSD': {
             $config_file = '/usr/local/etc/puppet/puppet.conf'
             $admingroup = 'wheel'
             $service_name = 'puppet'
+            # There are no official Puppetlabs packages for FreeBSD
+            $ssldir = '/var/puppet/ssl'
         }
         default: {
             fail("Unsupported operating system: ${::osfamily}/${::operatingsystem}")
